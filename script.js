6ba499df16bef30d3714c5a6b27321385a70415b
Brian Tran <briantrannn@gmail.com>
2024-09-13 20:15:29 -0500

    finished all basic functions. some bugs arise if doing something not meant to do but will work later on

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+const clearButton = document.querySelector(".clear");
+const display = document.querySelector(".display");
+const digits = document.querySelectorAll(".digit");
+const operators = document.querySelectorAll(".sign");
+const evaluate = document.querySelector(".evaluate");
+
+let displayText = "";
+let num1 = "";
+let num2 = "";
+let operator;
+let turn = true;
+
+
 function add(a, b) {
     return a + b;
 }
@@ -14,21 +27,80 @@ function multiply(a, b) {
     return a * b;
 }
 
-let num1;
-let num2;
-let operator;
 
 function operate(a, b, sign) {
+    let ans;
     if (sign == "+") {
-        add(a, b);
+        ans = add(a, b);
     }
     else if (sign == "-") {
-        subtract(a, b);
+        ans = subtract(a, b);
     }
-    else if (sign == "*") {
-        multiply(a, b);
+    else if (sign == "×") {
+        ans = multiply(a, b);
     }
-    if (sign == "/") {
-        divide(a, b);
+    if (sign == "÷") {
+        if (b == 0) {
+            return "NOT ALLOWED, PLEASE CLEAR";
+        }
+        ans = divide(a, b);
     }
+    return ans;
 }
+
+clearButton.addEventListener("click", () => {
+    displayText = "";
+    num1 = "";
+    num2 = "";
+    display.innerHTML = "0";
+    turn = true;
+});
+
+evaluate.addEventListener("click", () => {
+    if (num1 == "" || num2 == "") {
+        alert("ERROR");
+    }
+    num1Converted = Number(num1);
+    num2Converted = Number(num2);
+    num1 = operate(num1Converted, num2Converted, operator);
+    num1.toString();
+    num2 = "";
+    displayText = num1;
+    display.innerHTML = displayText;
+    turn = true;
+});
+
+digits.forEach(function(button) {
+    button.addEventListener("click", function() {
+        if (turn) {
+            num1 += this.innerHTML;
+            displayText += this.innerHTML;
+        } else {
+            num2 += this.innerHTML;
+            displayText += this.innerHTML;
+        }
+        display.innerHTML = displayText;
+    });
+});
+
+operators.forEach(function(button) {
+    button.addEventListener("click", function() {
+        if (turn) {
+            operator = this.innerHTML;
+            displayText += " " + operator + " ";
+            display.innerHTML = displayText;
+            turn = false;
+        }
+        else {
+            num1Converted = Number(num1);
+            num2Converted = Number(num2);
+            num1 = operate(num1Converted, num2Converted, operator);
+            num1.toString();
+            num2 = "";
+            operator = this.innerHTML;
+            displayText = num1 + " " + operator + " ";
+            display.innerHTML = displayText;
+            turn = false;
+        }
+    });
+});
